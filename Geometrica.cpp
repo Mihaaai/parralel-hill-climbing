4f0782ef4acc6099ab135ff2acd572cb3ea49496
Mihaaai <ghidome@gmail.com>
2019-02-08 09:22:10 +0200

    add output

--- Geometrica.cpp
+++ Geometrica.cpp
@@ -1,18 +1,41 @@
-#include "pch.h"
+#define RUN_ON_WINDOWS false
+/*EXPECTED FULL OUTPUT :
+Example with comments explaining lines
+{HILL CLIMBING LINE} = world_rank x y
+{HILL CLIMBING SECTION} = ({HILL CLIMBING LINE}\n)*
+world_size
+{HILL CLIMBING SECTION} // sequential algorithm
+BS best_score x y  //best score of sequential algorithm
+FS duration // final of sequential algorithm, followed by time duration
+{HILL CLIMBING SECTION} // parrallel algorithm
+BP best_score x y  //best score of parrallel algorithm
+FP duration // final of parralel algorithm, followed by time duration
+*/
+
+#if RUN_ON_WINDOWS
+	#include "pch.h"
+	#include <windows.h>
+#else 
+	#include <unistd.h>
+#endif
+
 #include <mpi.h>
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
 #include <iostream>
-#include <windows.h>
 
-
+#define MAX_ITERATIONS 1000
 #define DEBUG_EPOCHS false
 #define DEBUG_CANDIDATES false
-#define DEBUG_LAST_IMPROV true
-#define DEBUG_ITERATIONS true
+#define DEBUG_LAST_IMPROV false
+#define DEBUG_ITERATIONS false
 #define M_PI 3.1415
+#define DEBUG_OUTPUT false
+#define FULL_OUTPUT true
+#define FULL_OUTPUT_2 true
 
 using namespace std;
 
@@ -53,8 +76,8 @@ void printStatus(double currentPoint[2], double stepSize[2]) {
 		currentPoint[0], currentPoint[1], stepSize[0], stepSize[1]);
 }
 
-double* hillClimb(double *finalScore, int coordX, int coordY) {
-	Sleep(50);
+double* hillClimb(double *finalScore, int coordX, int coordY, bool doPrint) {
+	
 	// number of iterations with no moves before we conclude that we converged
 	int burnoutEpochs = 100;
 	double stepSize[2] = { 0.5, 0.5 };
@@ -72,12 +95,21 @@ double* hillClimb(double *finalScore, int coordX, int coordY) {
 	int epoch = 1;
 	int iterations = 0;
 	double lastImprovement = 0;
+
+	//get rank of current process
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	if(FULL_OUTPUT && doPrint){
+		printf("%d %.2f %.2f\n", world_rank, currentPoint[0], currentPoint[1]);	
+	}
+
 	while (true) {
 		// compute initial score
 		double before = func(currentPoint);
 		for (int i = 0; i < 2; ++i) {
 			int best = -1; //best candidate
-			double bestScore = -numeric_limits<double>::infinity();  // minus INF
+			double bestScore = - numeric_limits<double>::infinity();  // minus INF
 			double currentValue = currentPoint[i];
 			for (int j = 1; j <= 5; ++j) {
 				currentPoint[i] = currentPoint[i] + stepSize[i] * candidate(j, acceleration);
@@ -104,6 +136,9 @@ double* hillClimb(double *finalScore, int coordX, int coordY) {
 			else {
 				currentPoint[i] = currentPoint[i] + stepSize[i] * candidate(best, acceleration);
 				stepSize[i] = stepSize[i] * candidate(best, acceleration); // accelerate
+				if(FULL_OUTPUT && doPrint){
+					printf("%d %.2f %.2f\n", world_rank, currentPoint[0], currentPoint[1]);	
+				}
 			}
 
 		}
@@ -129,6 +164,9 @@ double* hillClimb(double *finalScore, int coordX, int coordY) {
 			epoch = 0;
 			lastImprovement = improvement;
 		}
+		if( iterations < MAX_ITERATIONS){
+			return currentPoint;
+		}
 
 	}
 }
@@ -136,18 +174,26 @@ double* hillClimb(double *finalScore, int coordX, int coordY) {
 //Sequential implementation for hill climbing
 void sequential_climbing(Point points[], int nr_points) {
 
-	printf("\nSequential climbing:\n");
+	if(DEBUG_OUTPUT){
+		printf("\nSequential climbing:\n");	
+	}
+	
 	double score;
 	double bestClimber[2], best_score = 0;
 	for (int i = 0; i < nr_points; i++) {
-		double *climber = hillClimb(&score, points[i].phi, points[i].p);
+		double *climber = hillClimb(&score, points[i].phi, points[i].p, false);
 		if (score > best_score) {
 			best_score = score;
 			bestClimber[0] = climber[0];
 			bestClimber[1] = climber[1];
 		}
 	}
-	printf("Finest climber at %.2f meters : (%.2f, %.2f)\n", best_score, bestClimber[0], bestClimber[1]);
+	if(FULL_OUTPUT){
+		printf("BS %.2f %.2f %.2f\n",best_score, bestClimber[0], bestClimber[1]);
+	}
+	if(DEBUG_OUTPUT){
+		printf("Finest climber at %.2f meters : (%.2f, %.2f)\n", best_score, bestClimber[0], bestClimber[1]);	
+	}
 }
 
 //Parallel implementation for hill climbing
@@ -167,7 +213,7 @@ void parallel_climbing(int world_rank, int world_size, Point points[], int nr_po
 				MPI_STATUS_IGNORE);
 
 			double score;
-			double *climber = hillClimb(&score, phi, p);
+			double *climber = hillClimb(&score, phi, p, true);
 
 			MPI_Send(&score, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 			MPI_Send(&climber[0], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
@@ -178,7 +224,7 @@ void parallel_climbing(int world_rank, int world_size, Point points[], int nr_po
 	//Master
 	else {
 		int contor = 0, process = 1;
-		double best_phi, best_p, best_score = 0;
+		double best_phi = -1, best_p = -1, best_score = 0;
 		//If there are more points than processes
 		if (nr_points >= world_size) {
 			//Where points are stil available
@@ -283,8 +329,13 @@ void parallel_climbing(int world_rank, int world_size, Point points[], int nr_po
 				MPI_Send(&scope, 1, MPI_DOUBLE, process, 0, MPI_COMM_WORLD);
 			}
 		}
+		if(DEBUG_OUTPUT){
+			printf("Finest climber at %.2f meters : (%.2f, %.2f)\n", best_score, best_phi, best_p);			
+		}
 
-		printf("Finest climber at %.2f meters : (%.2f, %.2f)\n", best_score, best_phi, best_p);
+		if(FULL_OUTPUT_2){
+			printf("BEP %.2f %.2f %.2f\n",best_score, best_phi, best_p);
+		}
 	}
 }
 
@@ -305,7 +356,7 @@ void perfect_parallel_climbing(int world_rank, int world_size, Point points[], i
 				MPI_STATUS_IGNORE);
 
 			double score;
-			double *climber = hillClimb(&score, phi, p);
+			double *climber = hillClimb(&score, phi, p, false);
 
 			MPI_Send(&score, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 			MPI_Send(&climber[0], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
@@ -315,14 +366,14 @@ void perfect_parallel_climbing(int world_rank, int world_size, Point points[], i
 	//Master
 	else {
 		int contor = 0, process = 1;
-		double best_phi, best_p, best_score = 0;
+		double best_phi = -1, best_p = -1, best_score = 0;
 
 		int rank = 0, *processes;
 		processes = new (nothrow) int[world_size];
 		for (int i = 1; i < world_size; i++) {
 			processes[i] = 0;
 		}
-		boolean is_score = false, is_phi = false;
+		bool is_score = false, is_phi = false;
 
 		//If there are more points than processes
 		if (nr_points >= world_size) {
@@ -437,8 +488,12 @@ void perfect_parallel_climbing(int world_rank, int world_size, Point points[], i
 			}
 			delete[] processes;
 		}
-
-		printf("Finest climber at %.2f meters : (%.2f, %.2f)\n", best_score, best_phi, best_p);
+		if(FULL_OUTPUT){
+			printf("BP %.2f %.2f %.2f\n",best_score, best_phi, best_p);
+		}
+		if(DEBUG_OUTPUT){
+			printf("Finest climber at %.2f meters : (%.2f, %.2f)\n", best_score, best_phi, best_p);	
+		}
 	}
 }
 
@@ -454,14 +509,19 @@ int main(int argc, char** argv) {
 
 	srand(time(NULL) + world_rank);
 
+	//get number of starting points
+	int nr_points = 0;
+	if(argc >= 2){
+		sscanf(argv[1], "%d", &nr_points);
+	}
+	
+
 	//Create points where climbers can start
 	Point points[20];
-	int nr_points = 0;
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < nr_points; i++) {
 		double phi = rand() % 20;
 		double p = rand() % 20;
 		points[i] = { phi, p };
-		nr_points++;
 	}
 	
 	//Sequential implementation for hill climbing
@@ -472,9 +532,15 @@ int main(int argc, char** argv) {
 		sequential_climbing(points, nr_points);
 		sequential_time_end = MPI_Wtime();
 
-		printf("Execution time for sequential climbing: %.4f seconds\n", sequential_time_end - sequential_time_start);
-		printf("\nParallel climbing:\n");
+		if(DEBUG_OUTPUT){
+			printf("Execution time for sequential climbing: %.4f seconds\n", sequential_time_end - sequential_time_start);	
+			printf("\nParallel climbing:\n");
+		}
+		if(FULL_OUTPUT){
+			printf("FS %.4f\n", sequential_time_end - sequential_time_start);  // Sequential algo duration
+		}
 	}
+
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
@@ -487,7 +553,10 @@ int main(int argc, char** argv) {
 
 	if (world_rank == 0) {
 		parallel_time_end = MPI_Wtime();
-		printf("Execution time for parallel climbing: %.4f seconds\n", parallel_time_end - parallel_time_start);
+		if(DEBUG_OUTPUT){
+			printf("Execution time for parallel climbing: %.4f seconds\n", parallel_time_end - parallel_time_start);	
+		}
+		
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -495,14 +564,22 @@ int main(int argc, char** argv) {
 	//Perfect parallel implementation for hill climbing
 	double parallel_time_start_perfect, parallel_time_end_perfect;
 	if (world_rank == 0) {
-		printf("\nPerfect parallel climbing:\n");
+		if(DEBUG_OUTPUT){
+			printf("\nPerfect parallel climbing:\n");	
+		}
 		parallel_time_start_perfect = MPI_Wtime();
 	}
 	perfect_parallel_climbing(world_rank, world_size, points, nr_points);
 
 	if (world_rank == 0) {
 		parallel_time_end_perfect = MPI_Wtime();
-		printf("Execution time for perfect parallel climbing: %.4f seconds\n", parallel_time_end_perfect - parallel_time_start_perfect);
+		if(FULL_OUTPUT){
+			printf("FP %.4f\n", parallel_time_end_perfect - parallel_time_start_perfect); // Parralel algo duration
+		}
+		if(DEBUG_OUTPUT){
+			printf("Execution time for perfect parallel climbing: %.4f seconds\n", parallel_time_end_perfect - parallel_time_start_perfect);	
+		}
+		
 	}
 
 	MPI_Finalize();
